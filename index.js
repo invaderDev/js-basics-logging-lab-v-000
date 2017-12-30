53efaeb749f6ed7d6279cf6691abca4b1c7b317d
Kris Thompson <kt.devcat@gmail.com>
2017-12-30 00:29:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const driver = 'bob';
 console.log('this code was called');
 console.log(driver);
+<<<<<<< HEAD
 console.log(driver, 'is the driver variable name');
+=======
+console.log(driver, 'is the driver variable name');
+>>>>>>> bb3a493f23152e8296d73033b7fd07c1ce7a41df
